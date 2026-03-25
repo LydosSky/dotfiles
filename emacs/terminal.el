d0422164dc3dc7924149e57d35eac65e26e0f0e0
Lydos <lydossky@gmail.com>
2026-03-25 12:42:10 +0300

    chore(emacs): deleting redundant emacs configuration files

--- emacs/terminal.el
+++
@@ -1,9 +0,0 @@
-;;; terminal.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
-
-(use-package vterm
-  :ensure t
-  :defer t
-  :commands vterm
-  :config
-  ;; Speed up vterm
-  (setq vterm-timer-delay 0.01))
